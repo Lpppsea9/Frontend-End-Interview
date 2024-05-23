96c69f96d93db390e2523e125e211e642a2a1d86
Lpppsea9 <lpppsea9@foxmail.com>
2024-05-23 21:20:39 +0800

    init

---
+++ TEST-Program/vue3-webpack/src/App.vue
@@ -0,0 +1,75 @@
+<template>
+	<div>
+		<!-- 多文件上传 -->
+		<span>多文件上传：</span>
+		<input type="file" name="file" @change="fileChange" multiple />
+		<span v-for="item in fileMutiList">{{ item.name }}</span>
+		<hr />
+		<!-- 切片上传 -->
+		<span>切片上传：</span>
+		<input type="file" name="file" @change="fileChange" />
+		<hr />
+
+		<button @click="submit">提交</button>
+		<!-- <button @click="increment">button</button> -->
+		<button @click="testBtn">测试按钮</button>
+		<hr />
+		<img style="width: 200px" :src="imgSrc" alt="imgAlt" />
+	</div>
+</template>
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+
+const count = ref(0);
+let imgSrc = ref("");
+const fileMutiList = ref([]);
+let _fileObj = null;
+// 测试按钮
+function testBtn() {
+	console.log(new File(["ppp"], "aa.text"));
+}
+// 文件上传
+function fileChange(e) {
+	// let file = e.target.files[0];
+	// console.log(file);
+	// if (file.size > 10 * 24 * 24) {
+	// 	alert("文件不能大于10M");
+	// }
+	// if (file.type !== "video/mp4") {
+	// 	alert("必须是mp4");
+	// }
+	// 转成blob对象
+	// let sliceBlob = new Blob([file]).slice(0, 2000);
+	// let silceFile = new File([sliceBlob], "fff.png");
+	// const fr = new FileReader();
+	// fr.readAsDataURL(silceFile);
+	// fr.onload = function () {
+	// 	console.log("fr", fr);
+	// 	const frRes = fr.result;
+	// 	imgSrc.value = frRes;
+	// };
+	// 怎么做缩略图，文本预览
+	// 多文件上传
+	/*if (e.target.files.length > 0) {
+		fileMutiList.value.push(e.target.files[0]);
+	} else {
+		fileMutiList.concat(e.target.files);
+	}*/
+	// 切片上传
+	_fileObj = e.target.files[0];
+}
+
+// 文件提交
+async function submit() {
+	// 多文件上传
+	/* fileMutiList.value.forEach((item) => {
+		let _formData = new FormData();
+		_formData.append(item.name + "file", item);
+		axios.post("/xxx", _formData);
+	}); */
+	// 切片上传
+	let siz;
+}
+</script>
+<style></style>
---
+++ TEST-Program/vue3-webpack/src/index.js
@@ -0,0 +1,23 @@
+import { sum } from "./math";
+// import "./style/style1.css";
+import "./css/index.css";
+import "./less/index.less";
+
+// 引入第三方模块
+// import _ from "lodash";
+
+const sumRes = sum(10, 20);
+console.log("sumRes", sumRes);
+
+// 引入图片
+// function insertImgElem(imgFile) {
+// 	const img = new Image();
+// 	img.src = imgFile;
+// 	document.body.appendChild(img);
+// }
+
+// for (let i = 0; i < 5; i++) {
+// 	setTimeout(() => {
+// 		console.log(i);
+// 	}, 1000);
+// }
