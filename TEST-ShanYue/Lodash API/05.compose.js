df097272c03e7f37063dfcbd8741fdb74bbf4300
Lpppsea9 <lpppsea9@foxmail.com>
2024-05-30 18:07:14 +0800

    feat: lodash-get

---
+++ TEST-ShanYue/Lodash API/05.compose.js
@@ -0,0 +1,12 @@
+// 实现一个 compose 函数，进行函数合成，比如 redux 中的 compose，react 高阶组件连续调用时的 compose
+
+const add10 = (x) => x + 10;
+const mul10 = (x) => x * 10;
+const add100 = (x) => x + 100;
+
+const compose =
+	(...fns) =>
+	(init) =>
+		fns.reverse().reduce((prev, cur) => cur(prev), init);
+
+console.log(compose(add10, mul10, add100)(10));
